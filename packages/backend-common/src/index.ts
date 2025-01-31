a00a5784488371761a8a2eeb35e815cbd12c9c3c
rahmatdeep <rahmatdeep@gmail.com>
2025-02-01 05:02:14 +0530

    fix env

--- packages/backend-common/src/index.ts
+++ packages/backend-common/src/index.ts
@@ -1,1 +1,22 @@
-export const JWT_SECRET = process.env.JWT_SECRET || "";
+import dotenv from "dotenv";
+import z from "zod";
+
+dotenv.config({ path: "../../.env" });
+
+const envSchema = z.object({
+  JWT_SECRET: z.string().min(1, "JWT_SECRET is required"),
+});
+
+const validateEnv = () => {
+  const parsed = envSchema.safeParse(process.env);
+
+  if (!parsed.success) {
+    console.error(`❌ Invalid environment variables: `, parsed.error.format());
+    throw new Error("Invalid environment variables");
+  }
+  return parsed.data;
+};
+
+export const env = validateEnv();
+
+export const { JWT_SECRET } = env;
