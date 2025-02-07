7258956177e8e49badceac4a8db9244000a4cc37
rahmatdeep <rahmatdeep@gmail.com>
2025-02-07 09:13:28 +0530

    separate canvas logic

--- apps/drawr-fe/tailwind.config.ts
+++ apps/drawr-fe/tailwind.config.ts
@@ -6,13 +6,13 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  // theme: {
-  //   extend: {
-  //     colors: {
-  //       background: "var(--background)",
-  //       foreground: "var(--foreground)",
-  //     },
-  //   },
-  // },
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
   plugins: [],
 } satisfies Config;
