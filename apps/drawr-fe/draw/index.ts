60bde25d86b271649eb3d00be68254515f983bae
rahmatdeep <rahmatdeep@gmail.com>
2025-02-18 14:20:06 +0530

    add logic for rendering previous shapes

--- apps/drawr-fe/draw/index.ts
+++ apps/drawr-fe/draw/index.ts
@@ -1,5 +1,22 @@
+type Shape =
+  | {
+      type: "rect";
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+    }
+  | {
+      type: "circle";
+      centerX: number;
+      centerY: number;
+      radius: number;
+    };
+
 export function initDraw(canvas: HTMLCanvasElement) {
   const ctx = canvas.getContext("2d");
+
+  const exsistingShapes: Shape[] = [];
 
   if (!ctx) {
     return;
@@ -16,18 +33,36 @@ export function initDraw(canvas: HTMLCanvasElement) {
     startX = e.clientX;
     startY = e.clientY;
   });
-  canvas.addEventListener("mouseup", () => {
+  canvas.addEventListener("mouseup", (e) => {
     clicked = false;
+    const width = e.clientX - startX;
+    const height = e.clientY - startY;
+    exsistingShapes.push({ type: "rect", x: startX, y: startY, height, width });
   });
   canvas.addEventListener("mousemove", (e) => {
     if (clicked) {
       const width = e.clientX - startX;
       const height = e.clientY - startY;
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = "rgba(0,0,0)";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      clearCanvas(exsistingShapes, canvas);
       ctx.strokeStyle = "rgba(255, 255, 255)";
       ctx.strokeRect(startX, startY, width, height);
+    }
+  });
+}
+
+function clearCanvas(exsistingShapes: Shape[], canvas: HTMLCanvasElement) {
+  const ctx = canvas.getContext("2d");
+  if (!ctx) return;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "rgba(0,0,0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = "rgba(255, 255, 255)";
+
+  exsistingShapes.map((shape) => {
+    if (shape.type === "rect") {
+      ctx.strokeStyle = "rgba(255, 255, 255)";
+      ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+    } else if (shape.type === "circle") {
     }
   });
 }
