e44480b89ce2cfa5efdcfefd95643f6c69956bfc
rahmatdeep <rahmatdeep@gmail.com>
2025-05-16 02:59:14 +0530

    add turn server urls

--- apps/drawr-fe/components/VoiceCallComponent.tsx
+++ apps/drawr-fe/components/VoiceCallComponent.tsx
@@ -204,6 +204,16 @@ export function VoiceCallComponent({
       iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
         { urls: "stun:stun1.l.google.com:19302" },
+        {
+          urls: "turn:openrelay.metered.ca:443",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443?transport=tcp",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
       ],
     });
 
