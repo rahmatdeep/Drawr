dc47be545f2f2a97dba13fd6e0c06d1c54501e0c
rahmatdeep <rahmatdeep@gmail.com>
2025-12-04 03:44:48 +0530

    fix comment

--- packages/backend-common/src/index.ts
+++ packages/backend-common/src/index.ts
@@ -11,7 +11,7 @@ const validateEnv = () => {
   const parsed = envSchema.safeParse(process.env);
 
   if (!parsed.success) {
-    console.error(`❌ Invalid environment variables: `, parsed.error.format());
+    console.error(`Invalid environment variables: `, parsed.error.format());
     throw new Error("Invalid environment variables");
   }
   return parsed.data;
